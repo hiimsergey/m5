afdc119a3a0203172d5ca0d7d2e2e55562fd3643
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-09-11 12:16:39 +0200

    Condition validation

--- src/parser.zig
+++ src/parser.zig
@@ -1,18 +1,19 @@
 const M5Error = @import("error.zig").M5Error;
 
 const State = enum(u8) {
-	Ok,
 	InExpression,
-	ExpectingExpr,
+	InNumber,
+	ExpectingExpression,
 	ExpectingOperator
 };
 
 pub fn validate(condition: []const u8) !void {
 	var indentation_level: usize = 0;
-	var state = State.ExpectingExpr;
+	var state = State.InExpression;
 
-	for (condition, 0..) |ch, i| {
-		switch (ch) {
+	var i: usize = 0;
+	while (i < condition.len) : (i += 1) {
+		switch (condition[i]) {
 			' ' => {
 				if (state == .InExpression) state = .ExpectingOperator;
 			},
@@ -26,15 +27,60 @@ pub fn validate(condition: []const u8) !void {
 					state != .InExpression) return M5Error.InvalidConditionSyntax;
 				indentation_level -= 1;
 			},
-			'0'...'9', 'a'...'z', 'A'...'Z', '_', '-' => {
-
+			'0'...'9' => {
+				switch (state) {
+					.ExpectingExpression => state = .InNumber,
+					.InExpression, .InNumber => continue,
+					.ExpectingOperator => return M5Error.InvalidConditionSyntax
+				}
 			},
-			// TODO operators
+			'a'...'z', 'A'...'Z', '_', '-' => {
+				switch (state) {
+					.InExpression => continue,
+					.ExpectingExpression => state = .InExpression,
+					.InNumber, .ExpectingOperator =>
+						return M5Error.InvalidConditionSyntax
+				}
+			},
+			'&', '|', '=' => {
+				switch (state) {
+					.ExpectingExpression => return M5Error.InvalidConditionSyntax,
+					.InExpression, .InNumber => {
+						if (indentation_level > 0) return M5Error.InvalidConditionSyntax;
+						state = .ExpectingExpression;
+					},
+					.ExpectingOperator => state = .ExpectingExpression
+				}
+			},
+			'<', '>' => {
+				switch (state) {
+					.ExpectingExpression => return M5Error.InvalidConditionSyntax,
+					.InExpression, .InNumber => {
+						if (indentation_level > 0) return M5Error.InvalidConditionSyntax;
+						state = .ExpectingExpression;
+					},
+					.ExpectingOperator => state = .ExpectingExpression
+				}
+				if (condition[i + 1] == '=') i += 1;
+			},
+			'!' => {
+				if (condition[i + 1] != '=') return M5Error.InvalidConditionSyntax;
+				i += 1;
+				switch (state) {
+					.ExpectingExpression => return M5Error.InvalidConditionSyntax,
+					.InExpression, .InNumber => {
+						if (indentation_level > 0) return M5Error.InvalidConditionSyntax;
+						state = .ExpectingExpression;
+					},
+					.ExpectingOperator => state = .ExpectingExpression
+				}
+			},
 			else => return M5Error.InvalidConditionSyntax
 		}
 	}
 
-	if (indentation_level > 0 or state != .Ok) return M5Error.InvalidConditionSyntax;
+	if (indentation_level > 0 or (state != .InExpression and state != .InNumber))
+		return M5Error.InvalidConditionSyntax;
 }
 
 pub fn parse(condition: []const u8) bool {
